1ae7a28d38c369e83750d58cad40128731a4b1ba
kou72 <suzukiko7273@gmail.com>
2024-01-05 01:33:30 +0900

    docusaurus.config.jsファイルのURLとbaseUrlを修正
    docusaurus.config.js
    - urlを"https://kou72.github.io"から"https://kou72.github.io/"に修正
    - baseUrlを"/ai_flash_card_doc"から"/ai_flash_card_doc/"に修正

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,11 +14,11 @@ const config = {
 
   // // Set the production url of your site here
   // url: 'https://your-docusaurus-site.example.com',
-  url: "https://kou72.github.io",
   // // Set the /<baseUrl>/ pathname under which your site is served
   // // For GitHub pages deployment, it is often '/<projectName>/'
   // baseUrl: "/",
-  baseUrl: "/ai_flash_card_doc",
+  url: "https://kou72.github.io/",
+  baseUrl: "/ai_flash_card_doc/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
