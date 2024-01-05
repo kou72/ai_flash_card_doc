e1ef5d9449b7684d2fee7ebf9f893f6512b193ed
kou72 <suzukiko7273@gmail.com>
2024-01-05 14:05:24 +0900

    index.jsファイルと関連する画像ファイルの追加、import文の修正、タグの修正
    src/pages/index.js
    - import文のクォーテーションをシングルクォーテーションからダブルクォーテーションに変更
    - import文のパスのスラッシュの向きを修正
    - Headingコンポーネントのimport文のクォーテーションをシングルクォーテーションからダブルクォーテーションに変更
    - heroBannerクラス名のクォーテーションをシングルクォーテーションからダブルクォーテーションに変更
    - 画像のパスを修正し、require関数を使って読み込むように変更
    - ボタンのリンク先をコメントアウトから実際のURLに変更
    - ボタンのリンク先をa要素で囲むように変更
    - 画像のリンク先をa要素で囲むように変更
    
    static/img/app-store-badge.png
    - 新規作成
    
    static/img/google-play-badge.png
    - 新規作成
    
    static/img/screenshot_card_list.png
    - 新規作成
    
    static/img/screenshot_test_play.png
    - 新規作成
    
    static/img/web-app-badge.png
    - 新規作成

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,27 +1,56 @@
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import clsx from "clsx";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Layout from "@theme/Layout";
+import HomepageFeatures from "@site/src/components/HomepageFeatures";
 
-import Heading from '@theme/Heading';
-import styles from './index.module.css';
+import Heading from "@theme/Heading";
+import styles from "./index.module.css";
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+    <header className={clsx("hero hero--primary", styles.heroBanner)}>
       <div className="container">
         <Heading as="h1" className="hero__title">
           {siteConfig.title}
         </Heading>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <img
+          src={require("@site/static/img/screenshot_test_play.png").default}
+          alt="undraw_docusaurus_tree.png"
+          width="200px"
+        />
         <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
+          <a
+            // href="https://apps.apple.com/jp/app/ai%E6%9A%97%E8%A8%98%E3%82%AB%E3%83%BC%E3%83%89/id1580596147"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src={require("@site/static/img/app-store-badge.png").default}
+              alt="app-store-badge.png"
+              height="54px"
+            />
+          </a>
+          <a
+            // href="https://play.google.com/store/apps/details?id=dev.skapp.ai_flash_card"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src={require("@site/static/img/google-play-badge.png").default}
+              alt="google-play-badge.png"
+              height="80px"
+            />
+          </a>
+          <a href="https://flash-pdf-card.web.app/" target="_blank" rel="noopener noreferrer">
+            <img
+              src={require("@site/static/img/web-app-badge.png").default}
+              alt="web-app-badge.png"
+              height="54px"
+            />
+          </a>
         </div>
       </div>
     </header>
@@ -29,11 +58,12 @@ function HomepageHeader() {
 }
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      description="Description will go into a meta tag in <head />"
+    >
       <HomepageHeader />
       <main>
         <HomepageFeatures />
