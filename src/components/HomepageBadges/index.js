83489f35c82ea47b50b807a77da33a694399f7b3
kou72 <suzukiko7273@gmail.com>
2024-01-14 17:52:55 +0900

    HomepageBadgesコンポーネントの実装を修正し、BadgeListとBadgeコンポーネントを追加
    src/components/HomepageBadges/index.js
    - a要素で囲まれたimg要素を3つ削除
    - BadgeListとBadgeコンポーネントを追加
    - BadgeListには3つのオブジェクトが含まれており、それぞれhrefとimgのプロパティを持つ
    - Badgeコンポーネントは、hrefとimgを受け取り、a要素で囲まれたimg要素を返す

--- src/components/HomepageBadges/index.js
+++ src/components/HomepageBadges/index.js
@@ -4,29 +4,32 @@ import styles from "./styles.module.css";
 export default function HomepageBadges() {
   return (
     <div className={styles.badges}>
-      <a
-        // href="https://apps.apple.com/jp/app/ai%E6%9A%97%E8%A8%98%E3%82%AB%E3%83%BC%E3%83%89/id1580596147"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <img
-          src={require("@site/static/img/app-store-badge.png").default}
-          alt="app-store-badge.png"
-        />
-      </a>
-      <a
-        // href="https://play.google.com/store/apps/details?id=dev.skapp.ai_flash_card"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <img
-          src={require("@site/static/img/google-play-badge.png").default}
-          alt="google-play-badge.png"
-        />
-      </a>
-      <a href="https://ankiapp.site/" target="_blank" rel="noopener noreferrer">
-        <img src={require("@site/static/img/web-app-badge.png").default} alt="web-app-badge.png" />
-      </a>
+      {BadgeList.map((props, idx) => (
+        <Badge key={idx} {...props} />
+      ))}
     </div>
+  );
+}
+
+const BadgeList = [
+  {
+    href: "https://apps.apple.com/jp/app/ai%E6%9A%97%E8%A8%98%E3%82%AB%E3%83%BC%E3%83%89/id1580596147",
+    img: "app-store-badge.png",
+  },
+  {
+    href: "https://play.google.com/store/apps/details?id=dev.skapp.ai_flash_card",
+    img: "google-play-badge.png",
+  },
+  {
+    href: "https://ankiapp.site/",
+    img: "web-app-badge.png",
+  },
+];
+
+function Badge({ href, img }) {
+  return (
+    <a href={href} target="_blank" rel="noopener noreferrer">
+      <img src={require("@site/static/img/" + img).default} alt={img} />
+    </a>
   );
 }
