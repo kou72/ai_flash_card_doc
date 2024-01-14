e4dedaf1498db0861d09ac1b6356b70049e1a38b
kou72 <suzukiko7273@gmail.com>
2024-01-14 22:50:35 +0900

    HomepageFeaturesコンポーネントの修正と画像の追加
    src/components/HomepageFeatures/index.js
    - h3要素のテキストを変更
    - p要素を追加して説明文を追加
    - 画像を追加して表示
    - div要素を追加してカード生成のボタンを表示
    
    src/components/HomepageFeatures/styles.module.css
    - paddingの値を変更
    
    static/img/homepagegif1.gif
    - 新しいファイルとして追加
    
    static/img/robot.png
    - 新しいファイルとして追加
    
    static/img/sampledoc.png
    - 新しいファイルとして追加

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -1,79 +1,40 @@
+import clsx from "clsx";
 import styles from "./styles.module.css";
 
 export default function HomepageFeatures() {
   return (
     <section className={styles.features}>
       <h2>Ai暗記カードの特徴</h2>
-      <h3>画像から暗記カードを生成できる</h3>
-      {/* <div className={styles.icon}>
-        <ImageSVG />
-        <RightArrowSVG />
-        <TagSVG />
-      </div> */}
+      <h3>写真をアップロード！あとはAIにお任せ！</h3>
+      <p className="text--center">
+        アップロードした画像のテキストから、自動で重要キーワードを抽出し、あっという間に暗記カードを生成。
+        <br />
+        暗記に集中して、学習効率をグッとアップさせましょう！
+      </p>
+      <img src={require("@site/static/img/robot.png").default} alt="robot.png" width="300px" />
+      <div>画像からカードを生成してみる</div>
+      <div className="container">
+        <div className="row">
+          <div className={clsx("col col--6")}>
+            <div className="text--center">
+              <img
+                src={require("@site/static/img/sampledoc.png").default}
+                alt="sampledoc.png"
+                height="300px"
+              />
+            </div>
+          </div>
+          <div className={clsx("col col--6")}>
+            <div className="text--center">
+              <img
+                src={require("@site/static/img/screenshot_test_play.png").default}
+                alt="screenshot_test_play.png"
+                width="200px"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
-
-const ImageSVG = () => {
-  return (
-    <div className={styles.ImageSVG}>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        strokeWidth={1.5}
-        stroke="currentColor"
-        className="w-6 h-6"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="m2.25 15.75 5.159-5.159a2.25 2.25 0 0 1 3.182 0l5.159 5.159m-1.5-1.5 1.409-1.409a2.25 2.25 0 0 1 3.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 0 0 1.5-1.5V6a1.5 1.5 0 0 0-1.5-1.5H3.75A1.5 1.5 0 0 0 2.25 6v12a1.5 1.5 0 0 0 1.5 1.5Zm10.5-11.25h.008v.008h-.008V8.25Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
-        />
-      </svg>
-    </div>
-  );
-};
-
-const RightArrowSVG = () => {
-  return (
-    <div className={styles.RightArrowSVG}>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        strokeWidth={1.5}
-        stroke="currentColor"
-        className="w-6 h-6"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3"
-        />
-      </svg>
-    </div>
-  );
-};
-
-const TagSVG = () => {
-  return (
-    <div className={styles.ImageSVG}>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        strokeWidth={1.5}
-        stroke="currentColor"
-        className="w-6 h-6"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M9.568 3H5.25A2.25 2.25 0 0 0 3 5.25v4.318c0 .597.237 1.17.659 1.591l9.581 9.581c.699.699 1.78.872 2.607.33a18.095 18.095 0 0 0 5.223-5.223c.542-.827.369-1.908-.33-2.607L11.16 3.66A2.25 2.25 0 0 0 9.568 3Z"
-        />
-        <path strokeLinecap="round" strokeLinejoin="round" d="M6 6h.008v.008H6V6Z" />
-      </svg>
-    </div>
-  );
-};
